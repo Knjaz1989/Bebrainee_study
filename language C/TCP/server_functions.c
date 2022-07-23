af6826c78a2728bc883ee9f950795a2ae1a0a1af
Igor Sverchkov <knjaz1989@gmail.com>
2022-07-23 19:45:57 +0300

    TCP_server changed

--- language C/TCP/server_functions.c
+++ language C/TCP/server_functions.c
@@ -29,7 +29,6 @@ json_object *get_argv (int argc, char **argv) {
                 break;
         }
     }
-    json_object_to_file("file.json", dict);
     char *collect[] = {"port", "host"};
     for (int index = 0; index < 2; index++) {
         json_object *object = json_object_object_get(dict, collect[index]);
